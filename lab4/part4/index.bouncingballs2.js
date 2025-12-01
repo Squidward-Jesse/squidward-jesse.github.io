376620539f0b3aafd14dafb59e2e4d86aa2f9835
Squidward-Jesse <jesse.littman@dcmail.ca>
2025-11-30 20:59:15 -0500

    Evilcircle added
    Created the user controlled circle that will move around the screen. Or as the MDN site calls it, the "EvilCircle".

--- lab4/part4/index.bouncingballs2.js
+++ lab4/part4/index.bouncingballs2.js
@@ -18,7 +18,17 @@ function randomRGB() {
     return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball 
+class Shape
+{constructor(x, y, velX, velY){
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    }
+
+}
+
+class Ball extends Shape
 {constructor(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
@@ -26,6 +36,7 @@ class Ball
     this.velY = velY;
     this.color = color;
     this.size = size;
+    this.exists = true;
 }
 
     draw() {
@@ -58,7 +69,7 @@ class Ball
 
     collisionDetect() {
     for (const ball of balls) {
-        if (this !== ball) {
+        if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -69,6 +80,73 @@ class Ball
       }
     }
   }
+}
+
+class EvilCircle extends Shape 
+{constructor(x, y){
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener('keydown', (e) => {
+        switch(e.key) {
+            case 'a':
+                this.x -= this.velX;
+                break;
+            case 'w':
+                this.y -= this.velY;
+                break;
+            case 's':
+                this.y += this.velY;
+                break;
+            case 'd':
+                this.x += this.velX;
+                break;
+        }
+    });
+}
+draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+checkBounds(){
+    if((this.x + this.size) >= width) {
+        this.x -= this.size;
+    }
+    if((this.x - this.size) <= 0) {
+        this.x += this.size;
+    }
+    if((this.y + this.size) >= height) {
+        this.y -= this.size;
+    }
+    if((this.y - this.size) <= 0) {
+        this.t += this.size;
+    }
+}
+collisionDetect() {
+    for (const ball of balls) {
+        if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+            ball.exists = false;
+            count--;
+            parseFloat.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
+
+
+
+
+
+
 }
 
 const balls = [];
